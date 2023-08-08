5795df6b6a1c6ad1a29b4138becafe9d9b76f231
Janosch Machowinski <J.Machowinski@cellumation.com>
2023-08-08 09:44:57 +0000

    Created example to demonstrate callback after handle drop

--- src/Client.cpp
+++ src/Client.cpp
@@ -1,21 +1,196 @@
-#include <rclcpp/node.hpp>
-#include <rclcpp/executors.hpp>
 #include <deadlock/action/some_action.hpp>
+#include <random>
+#include <rclcpp/executors.hpp>
+#include <rclcpp/node.hpp>
 #include <rclcpp_action/create_client.hpp>
 
-class ClientNode : public rclcpp::Node
+class ClientHandler
 {
 public:
-
     using Action = deadlock::action::SomeAction;
 
     using GoalHandle = rclcpp_action::ClientGoalHandle<Action>;
 
-
-//     rclcpp_action::Server<cm_interfaces::action::NodeStateChange>::SharedPtr cmdNodeStateActionServer;
-    ClientNode() : Node("DeadlockClient")
+    enum class ExecutionStatus
     {
-        actionClient = rclcpp_action::create_client<Action>(this, "/deadlock/example" );
+        goalSend,
+        executingPath,
+        goalReached,
+        error
+    };
+
+private:
+    mutable std::mutex interfaceMutex;
+
+    ExecutionStatus status;
+
+    size_t id;
+
+    bool cancelOngoing = false;
+
+    rclcpp::Logger logger;
+    rclcpp_action::Client<Action>::SharedPtr actionClient;
+
+    std::shared_future<GoalHandle::SharedPtr> pathRequest;
+    GoalHandle::SharedPtr execHandle;
+
+public:
+    ClientHandler(size_t id, rclcpp::Logger logger, rclcpp_action::Client<Action>::SharedPtr actionClient) :
+        id(id),
+        logger(logger),
+        actionClient(actionClient)
+    {
+        Action::Goal goal;
+        goal.id = id;
+
+        RCLCPP_INFO_STREAM(logger, "Setting new goal for ID " << id << " ptr is " << this);
+        auto sendGoalOptions = rclcpp_action::Client<Action>::SendGoalOptions();
+        sendGoalOptions.goal_response_callback =
+            std::bind(&ClientHandler::stateChangeGoalResponse, this, std::placeholders::_1);
+        sendGoalOptions.feedback_callback =
+            std::bind(&ClientHandler::stateChangeFeedback, this, std::placeholders::_1, std::placeholders::_2);
+        sendGoalOptions.result_callback = std::bind(&ClientHandler::stateChangeResult, this, std::placeholders::_1);
+
+        status = ExecutionStatus::goalSend;
+
+        pathRequest = actionClient->async_send_goal(goal, sendGoalOptions);
+    }
+
+    bool isRunning()
+    {
+        std::lock_guard<std::mutex> lk(interfaceMutex);
+        return status == ExecutionStatus::executingPath;
+    }
+
+    void dropHandle()
+    {
+        pathRequest = std::future<GoalHandle::SharedPtr>{};
+        execHandle.reset();
+    }
+
+    void cancelGoal()
+    {
+        // we need to create a second copy to the sharedptr in case the
+        // second thread jumps in and releases execHandle
+        GoalHandle::SharedPtr clientCopy;
+        {
+            std::lock_guard<std::mutex> lk(interfaceMutex);
+            cancelOngoing = true;
+            clientCopy = execHandle;
+        }
+
+        // this is a potential race condition
+        if (clientCopy)
+        {
+            actionClient->async_cancel_goal(clientCopy);
+            RCLCPP_INFO_STREAM(logger, "TrajectoryExecution " << id << ": Cancelation of goal by logic requested"
+                                                              << " ptr is " << this);
+        }
+
+        {
+            std::lock_guard<std::mutex> lk(interfaceMutex);
+            cancelOngoing = false;
+            execHandle.reset();
+            clientCopy.reset();
+        }
+    }
+
+    void stateChangeGoalResponse(const GoalHandle::SharedPtr& handle)
+    {
+        std::lock_guard<std::mutex> lk(interfaceMutex);
+        if (status != ExecutionStatus::goalSend)
+        {
+            RCLCPP_INFO_STREAM(
+                logger, "TrajectoryExecution::goalResponseCallback : Got goal response but did not send goal ? ID "
+                            << id << " ptr is " << this);
+            // we ignore any state transitions, if we are not actively planning, this should be error / cancel
+            // transitions
+            return;
+        }
+
+        if (execHandle && handle != execHandle)
+        {
+            // should never happen
+            throw std::runtime_error("Internal error, got feedback callback for non matching handle");
+        }
+
+        if (!handle)
+        {
+            status = ExecutionStatus::error;
+            pathRequest = std::future<GoalHandle::SharedPtr>{};
+            RCLCPP_INFO_STREAM(logger, "TrajectoryExecution" << id << ": Goal rejected");
+        }
+        else
+        {
+            execHandle = handle;
+            // lets clear the request, so that we are sure that we only hold one refernce to the handle
+            pathRequest = std::future<GoalHandle::SharedPtr>{};
+            status = ExecutionStatus::executingPath;
+            RCLCPP_INFO_STREAM(logger, "TrajectoryExecution" << id << ": Goal accepted"
+                                                             << " ptr is " << this);
+        }
+    }
+    void stateChangeResult(const GoalHandle::WrappedResult& result)
+    {
+        std::lock_guard<std::mutex> lk(interfaceMutex);
+        switch (result.code)
+        {
+            case rclcpp_action::ResultCode::UNKNOWN:
+                RCLCPP_INFO_STREAM(logger, "TrajectoryExecution " << id << ": Internal Error, action result UNKNOWN"
+                                                                  << " ptr is " << this);
+                break;
+            case rclcpp_action::ResultCode::SUCCEEDED:
+                RCLCPP_INFO_STREAM(logger, "TrajectoryExecution" << id << ": Reached target"
+                                                                 << " ptr is " << this);
+                break;
+            case rclcpp_action::ResultCode::CANCELED:
+                RCLCPP_INFO_STREAM(logger, "TrajectoryExecution " << id << ": action result CANCELED"
+                                                                  << " ptr is " << this);
+                break;
+            case rclcpp_action::ResultCode::ABORTED:
+                RCLCPP_INFO_STREAM(logger, "TrajectoryExecution " << id << ": action result ABORTED"
+                                                                  << " ptr is " << this);
+                break;
+        }
+        execHandle.reset();
+    }
+
+    void stateChangeFeedback(const GoalHandle::SharedPtr goalHandle,
+                             const std::shared_ptr<const Action::Feedback> /*feedback*/)
+    {
+        std::lock_guard<std::mutex> lk(interfaceMutex);
+        if (cancelOngoing)
+        {
+            RCLCPP_ERROR_STREAM(logger, "stateChangeFeedback for ID " << id << " hit race condition of cancel"
+                                                                      << " ptr is " << this);
+            return;
+        }
+
+        if (!execHandle)
+        {
+            RCLCPP_ERROR_STREAM(logger, "stateChangeFeedback for ID " << id << " called but handle was dropped"
+                                                                      << " ptr is " << this);
+        }
+
+        if (execHandle != goalHandle)
+        {
+            RCLCPP_ERROR_STREAM(logger, "stateChangeFeedback for ID " << id << " called with invalid handle"
+                                                                      << " ptr is " << this);
+        }
+    }
+};
+
+class ClientNode : public rclcpp::Node
+{
+public:
+    using Action = deadlock::action::SomeAction;
+
+    //     rclcpp_action::Server<cm_interfaces::action::NodeStateChange>::SharedPtr cmdNodeStateActionServer;
+    ClientNode() : Node("DeadlockClient"), gen(rd())
+    {
+        actionCbg = create_callback_group(rclcpp::CallbackGroupType::MutuallyExclusive);
+
+        actionClient = rclcpp_action::create_client<Action>(this, "/deadlock/example", actionCbg);
 
         RCLCPP_INFO_STREAM(get_logger(), "Waiting for action server");
         actionClient->wait_for_action_server();
@@ -23,55 +198,61 @@ public:
 
         actionClient->async_cancel_all_goals();
 
-        //note, the action is normaly created from a callback of a msg
-        timer = create_timer(std::chrono::milliseconds(100), std::bind(&ClientNode::createAction, this));
+        // note, the action is normaly created from a callback of a msg
+        createTimer = create_timer(std::chrono::milliseconds(100), std::bind(&ClientNode::createAction, this));
+        std::this_thread::sleep_for(std::chrono::microseconds(50));
+        cancelTimer = create_timer(std::chrono::milliseconds(1000), std::bind(&ClientNode::cancelAction, this));
 
+        activeClients.resize(30);
+        for (size_t i = 0; i < activeClients.size(); i++)
+        {
+            //             activeClients[i] = std::make_shared<ClientHandler>(i, get_logger(), actionClient);
+        }
     }
-
-    void stateChangeGoalResponse(const GoalHandle::SharedPtr& goalHandle)
-    {
-        handle = goalHandle;
-    }
-    void stateChangeResult(const GoalHandle::WrappedResult& result)
-    {
-    }
-    void stateChangeFeedback(
-        const GoalHandle::SharedPtr goalHandle, const std::shared_ptr<const Action::Feedback> feedback)
-    {
-    }
-
 
     void createAction()
     {
-        if(handle)
+        std::uniform_int_distribution<> distrib(0, activeClients.size() - 1);
+
+        size_t id = distrib(gen);
+
+        // save the old handle to keep it alive during application of new path
+        std::shared_ptr<ClientHandler> ptr = activeClients[id];
+
+        activeClients[id] = std::make_shared<ClientHandler>(id, get_logger(), actionClient);
+
+        if (ptr)
         {
-            return;
+            ptr->dropHandle();
         }
+    }
 
-        Action::Goal goal;
-        goal.force_control = true;
+    void cancelAction()
+    {
+        std::uniform_int_distribution<> distrib(0, activeClients.size() - 1);
 
-        RCLCPP_INFO_STREAM(get_logger(), "Setting new goal.");
-        auto sendGoalOptions = rclcpp_action::Client<Action>::SendGoalOptions();
-        sendGoalOptions.goal_response_callback =
-            std::bind(&ClientNode::stateChangeGoalResponse, this, std::placeholders::_1);
-        sendGoalOptions.feedback_callback = std::bind(&ClientNode::stateChangeFeedback, this, std::placeholders::_1,
-                                                    std::placeholders::_2);
-        sendGoalOptions.result_callback = std::bind(&ClientNode::stateChangeResult, this, std::placeholders::_1);
+        size_t id = distrib(gen);
 
-        actionClient->async_send_goal(goal, sendGoalOptions);
+        if (activeClients[id] && activeClients[id]->isRunning())
+        {
+            activeClients[id]->cancelGoal();
+        }
     }
 
 private:
+    std::random_device rd; // a seed source for the random number engine
+    std::mt19937 gen;
 
-    GoalHandle::SharedPtr handle;
+    rclcpp::CallbackGroup::SharedPtr actionCbg;
 
-    rclcpp::TimerBase::SharedPtr timer;
+    rclcpp::TimerBase::SharedPtr createTimer;
+    rclcpp::TimerBase::SharedPtr cancelTimer;
     rclcpp_action::Client<Action>::SharedPtr actionClient;
 
+    std::vector<std::shared_ptr<ClientHandler>> activeClients;
 };
 
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
     rclcpp::init(argc, argv);
 
--- src/Server.cpp
+++ src/Server.cpp
@@ -15,36 +15,43 @@ public:
 
     ServerNode() : Node("DeadlockServer")
     {
+        goalHandles.resize(30);
+
         actionServer = rclcpp_action::create_server<deadlock::action::SomeAction>(
                             this, "/deadlock/example",
                             std::bind(&ServerNode::handleGoal, this, std::placeholders::_1, std::placeholders::_2),
                             std::bind(&ServerNode::cancelGoal, this, std::placeholders::_1),
                             std::bind(&ServerNode::acceptGoal, this, std::placeholders::_1));
 
-        timer = create_timer(std::chrono::milliseconds(100), std::bind(&ServerNode::feedbackTimer, this));
+        timer = create_timer(std::chrono::milliseconds(10), std::bind(&ServerNode::feedbackTimer, this));
+
+        RCLCPP_INFO_STREAM(get_logger(), "ServerNode Alive");
     }
 
 
     void feedbackTimer()
     {
-        if(activeGoal)
+        for(auto &gPtr : goalHandles)
         {
-            RCLCPP_INFO_STREAM(get_logger(), "Sending Feedback");
+            if(gPtr)
+            {
+    //             RCLCPP_INFO_STREAM(get_logger(), "Sending Feedback");
 
-            auto fb = std::make_shared<Action::Feedback>();
-            fb->still_alive = true;
-            activeGoal->publish_feedback(fb);
+                auto fb = std::make_shared<Action::Feedback>();
+                fb->still_alive = true;
+                gPtr->publish_feedback(fb);
+            }
         }
     }
 
     rclcpp_action::GoalResponse handleGoal(const rclcpp_action::GoalUUID& /*uuid*/,
                                                 std::shared_ptr<const deadlock::action::SomeAction::Goal> /*goal*/)
     {
-        if(activeGoal)
-        {
-            RCLCPP_INFO_STREAM(get_logger(), "Rejected Goal, had already a handle");
-            return rclcpp_action::GoalResponse::REJECT;
-        }
+//         if(activeGoal)
+//         {
+//             RCLCPP_INFO_STREAM(get_logger(), "Rejected Goal, had already a handle");
+//             return rclcpp_action::GoalResponse::REJECT;
+//         }
 
         RCLCPP_INFO_STREAM(get_logger(), "Accepted Goal");
 
@@ -53,20 +60,29 @@ public:
 
     rclcpp_action::CancelResponse cancelGoal(const std::shared_ptr<GoalHandle> goalHandle)
     {
-        RCLCPP_INFO_STREAM(get_logger(), "Canceled active goal");
-        auto res = std::make_shared<Action::Result>();
-        res->was_forced_off = false;
-        goalHandle->canceled(res);
 
-        activeGoal.reset();
+
+        RCLCPP_INFO_STREAM(get_logger(), "Canceled active goal");
+//         auto res = std::make_shared<Action::Result>();
+//         res->was_forced_off = false;
+//         goalHandle->canceled(res);
+
+        goalHandles[goalHandle->get_goal()->id].reset();
 
         return rclcpp_action::CancelResponse::ACCEPT;
     }
 
     void acceptGoal(std::shared_ptr<GoalHandle> goalHandle)
     {
+        size_t id = goalHandle->get_goal()->id;
+
         RCLCPP_INFO_STREAM(get_logger(), "Goal activated");
-        activeGoal = goalHandle;
+        if(goalHandles[id])
+        {
+            goalHandles[id]->abort(std::make_shared<Action::Result>());
+            goalHandles[id].reset();
+        }
+        goalHandles[id] = goalHandle;
     }
 
 private:
@@ -75,8 +91,7 @@ private:
 
     rclcpp_action::Server<Action>::SharedPtr actionServer;
 
-    std::shared_ptr<GoalHandle> activeGoal;
-
+    std::vector<std::shared_ptr<GoalHandle>> goalHandles;
 };
 
 int main(int argc, char **argv)
